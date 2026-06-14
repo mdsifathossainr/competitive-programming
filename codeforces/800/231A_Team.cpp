704ed1333794926fceecef629403ff07f61105c4
mdsifathossainr <mdsifathossainr@gmail.com>
2026-06-14 13:33:55 +0600

    solve: Codeforces 231A Team problem

---
+++ codeforces/800/231A_Team.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: A. Team
+Link: https://codeforces.com/problemset/problem/231/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int count = 0;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a+b+c >= 2) count ++;
+    }
+    
+    cout << count << endl;
+
+    return 0;
+}
