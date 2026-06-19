e8df0e8a7586ca615165f5bc61b35fe5e960efce
mdsifathossainr <mdsifathossainr@gmail.com>
2026-06-19 12:35:31 +0600

    Solve: Codeforces 50A Domino Piling

---
+++ codeforces/800/50A_domino_piling.cpp
@@ -0,0 +1,19 @@
+/*
+Problem: Codeforces 50A - Domino piling
+Link: https://codeforces.com/problemset/problem/50/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+
+    int a = m * n;
+
+    int result = a / 2;
+
+    cout << result << endl;
+    return 0;
+}
