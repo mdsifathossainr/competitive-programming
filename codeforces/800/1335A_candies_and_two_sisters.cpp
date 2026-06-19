0db15f0293dc0f758874c4ee1dd0e0f65b73a1db
mdsifathossainr <mdsifathossainr@gmail.com>
2026-06-19 12:28:06 +0600

    Solve: Codeforces 1335A Candies and Two Sisters

---
+++ codeforces/800/1335A_candies_and_two_sisters.cpp
@@ -0,0 +1,32 @@
+/*
+Problem: Codeforces 1335A - Candies and Two Sisters
+Link: https://codeforces.com/problemset/problem/1335/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n <= 2)
+        {
+            cout << 0 << endl;
+        }
+        else if (n % 2 == 0)
+        {
+            cout << (n / 2) - 1 << endl;
+        }
+        else
+        {
+            cout << n / 2 << endl;
+        }
+    }
+    return 0;
+}
