39c3f1967ca20287edc31ad55deb21afe31529ee
mdsifathossainr <mdsifathossainr@gmail.com>
2026-06-20 12:45:19 +0600

    Solve: Codeforces 268A Games

---
+++ codeforces/800/268A_Games.cpp
@@ -0,0 +1,36 @@
+/*
+Problem: Codeforces 268A - Games
+Link: https://codeforces.com/problemset/problem/268/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> home(n);
+    vector<int> guest(n);
+    for(int i=0 ; i<n ; i++)
+    {
+        cin >> home[i] >> guest[i];
+    }
+
+    int ans = 0;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        for(int j=0 ; j<n ; j++)
+        {
+            if(home[i] == guest[j])
+            {
+                ans ++;
+            }
+        }
+    }
+    
+    cout << ans << endl;
+
+    return 0;
+}
+
