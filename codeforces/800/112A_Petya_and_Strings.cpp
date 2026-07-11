11b8cba39173174ac02a7efdb8ac0d52107d85ee
mdsifathossainr <mdsifathossainr@gmail.com>
2026-07-11 11:16:01 +0600

    Solve Codeforces 112A - Petya and Strings

---
+++ codeforces/800/112A_Petya_and_Strings.cpp
@@ -0,0 +1,30 @@
+/*
+Problem: Codeforces 112A - Petya and Strings
+Link: https://codeforces.com/problemset/problem/112/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1;
+    cin >> s1;
+
+    string s2;
+    cin >> s2;
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        s1[i] = tolower(s1[i]);
+        s2[i] = tolower(s2[i]);
+    }
+
+    if (s1 == s2)
+        cout << "0" << endl;
+    else if (s1 > s2)
+        cout << "1" << endl;
+    else
+        cout << "-1" << endl;
+
+    return 0;
+}
