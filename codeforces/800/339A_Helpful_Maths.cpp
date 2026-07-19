20292b0b2d674235ba7fc46d41e1e429621b86ab
mdsifathossainr <mdsifathossainr@gmail.com>
2026-07-19 12:31:25 +0600

    Solve: Codeforces 339A - Helpful Maths

---
+++ codeforces/800/339A_Helpful_Maths.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+    string s2;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isdigit(s[i]))
+        {
+            s2.push_back(s[i]);
+        }
+    }
+
+    sort(s2.begin(), s2.end());
+
+    for (int i = 0; i < s2.size(); i++)
+    {
+        if (i == s2.size() - 1)
+        {
+            cout << s2[i];
+        }
+        else
+        {
+            cout << s2[i] << "+";
+        }
+    }
+    return 0;
+}
