589f67e1f77fe98b32c581384cb09e3092424fbb
mdsifathossainr <mdsifathossainr@gmail.com>
2026-07-27 13:58:31 +0600

    Solve: Codeforces 617A - Elephant

---
+++ codeforces/800/617A_Elephant.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Codeforces 617A - Elephant
+Link: https://codeforces.com/problemset/problem/617/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int steps = 0;
+    if (n % 5 == 0)
+    {
+        steps = n / 5;
+        cout << steps << endl;
+    }
+    else
+    {
+        steps = (n / 5) + 1;
+        cout << steps << endl;
+    }
+
+    return 0;
+}
