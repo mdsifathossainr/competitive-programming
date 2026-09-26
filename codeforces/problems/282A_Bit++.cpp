dcc79256f8e342ee24e03bf28faebb0861fcfced
mdsifathossainr <mdsifathossainr@gmail.com>
2026-09-26 11:55:25 +0600

    Solve Codeforces 282A - Bit++

---
+++ codeforces/problems/282A_Bit++.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/282/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int x = 0;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+
+        if (s == "++X" || s == "X++")
+            x++;
+        else
+            x--;
+    }
+
+    cout << x;
+
+    return 0;
+}
